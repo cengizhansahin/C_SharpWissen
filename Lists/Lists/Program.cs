348c5abc1464e291669a81d043ba27fb722adaf3
Cengizhan Şahin <cengizhan7sahin@gmail.com>
2023-11-23 21:54:33 +0300

    atm uygulaması başlandı

--- Lists/Lists/Program.cs
+++ Lists/Lists/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Listler collectionlar olarak adlandırılan yapının bir elemanıdır.
             List<int> sayilar = new List<int>();
@@ -16,8 +16,8 @@
             //    Console.WriteLine(item);
             //}
 
-            //int[] x = { 2, 4554, 75, 7 };
-            //sayilar.AddRange(x);
+            int[] x = { 4, 4554, 75, 7 };
+            sayilar.AddRange(x);
             //sayilar.AddRange(new int[] {5,9,12});
 
             //foreach (var item in sayilar)
@@ -32,7 +32,95 @@
             //bool isExist = sayilar.Contains(1);
             //Console.WriteLine(isExist);
 
+            //sayilar.Clear();
+            //Console.WriteLine(sayilar.Count);
 
+            //bool isExist = sayilar.Exists(x => x > 10);
+            //Console.WriteLine(isExist);
+
+            // Koşulu sağlayan ilk elemanı döner, eğer koşul sağlanmazsa koşul sıfır döner.
+            //int falanFilan = sayilar.Find(x => x > 2);
+            //Console.WriteLine(falanFilan);
+
+            //Eğer ki hepsini getirmesini istiyorsak dönen verinin liste tipinde döneceğini bilmemiz gerekir. Bu durum için kullanacağımız metod finall metodudur.
+            //List<int> bulunanlar = sayilar.FindAll(x => x > 50);
+            //foreach (var item in bulunanlar)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // aradığımız elemanın indexini bulmak için
+            //int index = sayilar.IndexOf(1);
+            //Console.WriteLine(index);
+
+            //sayilar.Insert(3, 999);
+            //foreach (var item in sayilar)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //belirli bir elamanı silmek için
+            //sayilar.Remove(999);
+            //foreach (var item in sayilar)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //belirli bir indeksteki elemanı silmek için
+            //sayilar.RemoveAt(2);
+            //foreach (var item in sayilar)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //sayilar.ForEach(x=>Console.WriteLine(x));
+
+            //sayilar.Sort();
+            //sayilar.Reverse();
+            //sayilar.ForEach(x=>Console.WriteLine(x));
+
+            //int[] zibam = sayilar.ToArray();
+
+            Console.WriteLine("***********************");
+            Console.WriteLine("1) Para Çekme\n2) Bakiye Göster\n3) Para Yatırma\n4) Çıkış");
+            Console.WriteLine("Lütfen yapmak istediğiniz işlem numarasını giriniz: ");
+            int toplamBakiye = 5000;
+            int islemNumarasi = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (islemNumarasi==1)
+                {
+                    toplamBakiye -= ParaCekme();
+                    Console.WriteLine($"Çekilen para mitarı: {ParaCekme()}, kalan tutar: {toplamBakiye}");
+
+                }
+                else if (islemNumarasi==2)
+                {
+                    Console.WriteLine($"Bakiyeniz: {toplamBakiye}");
+                }
+                else if (islemNumarasi == 3)
+                {
+                    toplamBakiye += ParaYukle();
+                    Console.WriteLine($"Yüklenen para mitarı: {ParaYukle()}, yeni tutar: {toplamBakiye}");
+                }else if (islemNumarasi == 4)
+                {
+                    return;
+                }
+            }
+
+        }
+        public static int ParaCekme()
+        {
+            Console.WriteLine("Çekmek istediğiniz miktarı giriniz: ");
+            int miktar = Convert.ToInt32(Console.ReadLine());
+            return miktar;
+
+        }
+        public static int ParaYukle()
+        {
+            Console.WriteLine("Eklemek istediğiniz miktarı giriniz: ");
+            int miktar = Convert.ToInt32(Console.ReadLine());
+            return miktar;
         }
     }
 }
--- Lists/Lists/obj/Debug/net7.0/Lists.AssemblyInfo.cs
+++ Lists/Lists/obj/Debug/net7.0/Lists.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lists")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e162a7a44b7f8462b7cb15d7dabb555bca783af1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06eba734490067578c1e73339bf5aa83d3282556")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lists")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lists")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
