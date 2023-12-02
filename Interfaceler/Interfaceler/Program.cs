4ffdec1207f04c92a00fadba29c97dc67609d0e7
Cengizhan Şahin <cengizhan7sahin@gmail.com>
2023-12-02 14:38:58 +0300

    interfaceler işlendi

---
+++ Interfaceler/Interfaceler/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Interfaceler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Interfaceler (Arayüzler), genelde interfaceleri sözleşme şeklinde kodlarlar. içerisinde barındırdığı metodlar kesinlikle abstract classlardaki gibi kullanılmak zorundadır. Fakat nasıl kullanılacağnı belirtmezler. Interfacelerde access 
+            //Orangutan o1 = new Orangutan();
+            //o1.Speak();
+            //BalPorsugu balPorsugu = new BalPorsugu();
+            //balPorsugu.Speak();
+            TV tV = new TV("OLED TV", "LG");
+            CepTelefonu cepTelefonu = new CepTelefonu("Iphone 15 pro", "Apple");
+
+            tV.BilgileriGoster();
+            cepTelefonu.BilgileriGoster();
+        }
+    }
+    //interface ISpeakable
+    //{
+    //    void Speak();
+    //}
+    //public class Orangutan : ISpeakable
+    //{
+    //    public void Speak()
+    //    {
+    //        Console.WriteLine("Grawh");
+    //    }
+    //}
+    //public class BalPorsugu : ISpeakable
+    //{
+    //    public void Speak() { Console.WriteLine("Skrrt"); }
+    //}
+
+    interface IElektronikCİhaz
+    {
+        public string Model { get; set; }
+        public string Marka { get; set; }
+        void BilgileriGoster();
+    }
+    public class TV : IElektronikCİhaz
+    {
+        public string Model { get; set; }
+        public string Marka { get; set; }
+        public TV(string model, string marka)
+        {
+            Model = model;
+            Marka = marka;
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Televizyon Marka: {Marka}, Model: {Model}");
+        }
+    }
+    public class CepTelefonu : IElektronikCİhaz
+    {
+        public string Model { get; set; }
+        public string Marka { get; set; }
+        public CepTelefonu(string model, string marka)
+        {
+            Model = model;
+            Marka = marka;
+        }
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Telefon Marka: {Marka}, Model: {Model}"); ;
+        }
+    }
+}
